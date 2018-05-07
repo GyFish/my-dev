bbd7ff72362eca5f882ef611ecfe4e5dc7e13d1a
GyFish <geyu2227@163.com>
2018-05-07 23:45:09 +0800

    code mirror

--- dev/src/views/api-editor/index.vue
+++ dev/src/views/api-editor/index.vue
@@ -1,28 +1,46 @@
 <template>
 <div>
 
-  <div id="editor"></div>
+  <codemirror v-model="code" :options="cmOptions"></codemirror>
 
 </div>
 </template>
 
 <script>
-import SwaggerEditorBundle from 'swagger-editor-dist/swagger-editor-bundle'
-import SwaggerEditorStandalonePreset from 'swagger-editor-dist/swagger-editor-standalone-preset'
-import 'swagger-editor-dist/swagger-editor.css'
+
+// require component
+import { codemirror } from 'vue-codemirror'
+// require styles
+import 'codemirror/lib/codemirror.css'
+// language js
+import 'codemirror/mode/javascript/javascript.js'
+// theme css
+import 'codemirror/theme/dracula.css'
 
 export default {
+
   name: 'Api-Editor',
+
+  components: {
+    codemirror
+  },
+
+  data() {
+    return {
+      code: 'const a = 10',
+      cmOptions: {
+        // codemirror options
+        tabSize: 4,
+        mode: 'text/javascript',
+        theme: 'dracula',
+        lineNumbers: true,
+        line: true,
+      }
+    }
+  },
+
   mounted() {
-    // Build a system
-    SwaggerEditorBundle({
-      dom_id: '#editor',
-      layout: 'StandaloneLayout',
-      presets: [
-        SwaggerEditorStandalonePreset
-      ],
-      url: "http://127.0.0.1:7001/public/user.yaml",
-    })
   }
+
 }
 </script>
